4e9c1d6dea4f5330f3b56e4e0fafdde51418de98
Elijah Hoole <elihoole@gmail.com>
2019-07-31 01:15:43 +0530

    updated model with low val loss

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1-OyQJjFYNkXr9yIjiGYLiuBfxMr-ebLc'
 export_file_name = 'export.pkl'
 
-classes = ['early blight', 'late blight', 'healthy']
+classes = ['early-blight', 'late-blight', 'healthy']
 path = Path(__file__).parent
 
 app = Starlette()
